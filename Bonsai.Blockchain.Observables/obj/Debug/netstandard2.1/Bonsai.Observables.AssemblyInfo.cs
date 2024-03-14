3c6e1a1e95b80f45ec105cad2a8a8b23f8dff48e
rah <richard.hein@cognilore.com>
2024-03-14 11:42:22 -0400

    rebrand e

--- Bonsai.Blockchain.Observables/obj/Debug/netstandard2.1/Bonsai.Observables.AssemblyInfo.cs
+++ Bonsai.Blockchain.Observables/obj/Debug/netstandard2.1/Bonsai.Observables.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bonsai.Observables")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7dcace9fc7dbc6b92acd0134d90f21ba8ba7adc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d365b248e625a00fec45ae8577593863a1958fa6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bonsai.Observables")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bonsai.Observables")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
